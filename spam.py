7b5d96346465c425d3f8a80de02f4f71b25b39af
rusterun <62208960+rusterun@users.noreply.github.com>
2021-04-08 22:17:09 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,8 +1,9 @@
 import vk_api, random, requests
 from dfgo import dbinsert, dboutput
+from conf import TOKEN
 #импорт
 
-vk_session = vk_api.VkApi(token="93bb564df2600c56bc902a86c6a130ded07afb7eac1eb9772c200b9215ea894beafa0a18ecefcbbc49b5e")
+vk_session = vk_api.VkApi(token=TOKEN)
 session_api = vk_session.get_api()
 def send(message, peer_id):
     vk_session.method('messages.send', {'peer_id': peer_id, 'message': message, 'random_id': random.randint(-2147483648, +214783648)})
@@ -10,5 +11,3 @@ def send(message, peer_id):
 def send_subscribes(mess="Fox проехала"):
     for id in dboutput('subscribes'):
         send(mess, id['id'])
-
-print("Бот(3) запущен") #Пишем в консоль, чтобы знать, что проблем с авторизацией не было.
